08781ad62d02bfb8f24a3e38f37d2f0d9d2000d3
EternalRival <eternalrival8@gmail.com>
2023-02-05 19:52:27 +0300

    feat: protect user deletion with jwt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { compare } from 'bcrypt';
 import { Response } from 'express';
-import { AccessToken } from '../types/auth';
+import { IJwtToken, IJwtPayload } from '../types/auth';
 import { LoginUserDto } from '../users/user/login-user.dto';
 import { User } from '../users/user/user.entity';
 import { UsersService } from '../users/users.service';
@@ -14,28 +14,30 @@ export class AuthService {
   public async validateUser(login: LoginUserDto): Promise<User> {
     const user = await this.usersService.findUserByUsername(login.username);
     if (user && (await compare(login.password, user.password))) {
-      delete user.password;
       return user;
     }
     return null;
   }
 
-  public async generateToken(username: User['username']): Promise<AccessToken> {
-    const entity = await this.usersService.findUserByUsername(username);
-    const payload = { sub: entity.id, image: entity.image, username: entity.username };
-    return this.jwtService.sign(payload);
+  public generateJwtPayload(data: { id: number; image: string; username: string }): IJwtPayload {
+    return { sub: data.id, image: data.image, username: data.username };
   }
 
-  public login(login: LoginUserDto): Promise<AccessToken> {
-    const token = this.generateToken(login.username);
-    return token;
-  }
-  public refreshToken(login: LoginUserDto): Promise<AccessToken> {
-    const token = this.generateToken(login.username);
-    return token;
+  public async generateToken(id: User['id']): Promise<IJwtToken> {
+    const entity: User = await this.usersService.findUserById(id);
+    const payload: IJwtPayload = this.generateJwtPayload(entity);
+    return { access_token: this.jwtService.sign(payload) };
   }
 
-  public setCookies(response: Response, token: AccessToken, hours: number): void {
-    response.cookie('access_token', token, { maxAge: 3600000 * hours, httpOnly: true });
+  public login(login: User): Promise<IJwtToken> {
+    return this.generateToken(login.id);
+  }
+  public refreshToken(id: User['id']): Promise<IJwtToken> {
+    return this.generateToken(id);
+  }
+
+  public setCookies(response: Response, token: IJwtToken, hours: number): void {
+    const key = 'access_token';
+    response.cookie(key, token[key], { maxAge: 3600000 * hours, httpOnly: true });
   }
 }
