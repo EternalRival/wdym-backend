b3abd382015dae5ac76c34655c254e1567fa1829
EternalRival <eternalrival8@gmail.com>
2023-02-08 15:17:29 +0300

    feat: add lobbies resource

---
+++ src/lobbies/lobbies.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+import { LobbiesService } from './lobbies.service';
+import { LobbiesGateway } from './lobbies.gateway';
+
+@Module({
+  providers: [LobbiesGateway, LobbiesService]
+})
+export class LobbiesModule {}
