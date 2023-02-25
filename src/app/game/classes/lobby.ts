379fd9f0c4cc53256110ac4f8b88e72cb3b7befc
EternalRival <eternalrival8@gmail.com>
2023-02-25 10:37:47 +0300

    feat: add update situation method

--- src/app/game/classes/lobby.ts
+++ src/app/game/classes/lobby.ts
@@ -70,6 +70,12 @@ export class Lobby {
       }
     });
   }
+  public updateSituation(): void {
+    const choices = this.getChoices('situation');
+    const entries = Object.entries(choices);
+    const [winner] = entries.sort(([, a], [, b]) => (a.length > b.length ? -1 : 1));
+    this.situations.setPlayed(winner[0]);
+  }
   public reset(): void {
     const isHardReset = this.phase.current === GamePhase.PREPARE;
     this.players.list.forEach((player) => {
